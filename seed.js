91665370703dc7ffc07d41c2255d64c60e56cfb6
Norber <nox_nox_@hotmail.com>
2021-12-22 01:45:56 +0000

    Officers and departments seed

--- seed.js
+++ seed.js
@@ -1,7 +1,6 @@
 const seeder = require('mongoose-seed')
 const bcrypt = require('bcrypt')
 const mongoose = require('mongoose')
-const OwnersModel = require('./api/models/owners.model')
 require('dotenv').config()
 
 const db = process.env.MONGO_URL + process.env.MONGO_DB
@@ -9,13 +8,20 @@ const db = process.env.MONGO_URL + process.env.MONGO_DB
 console.log('>>> Seeding...')
 
 seeder.connect(db, function () {
-  seeder.loadModels(['./api/models/owners.model.js'])
-  seeder.clearModels(['owners'], function () {
+  seeder.loadModels([
+    './api/models/owners.model.js',
+    './api/models/police_officers.model.js',
+    './api/models/departments.model.js',
+  ])
+  seeder.clearModels(['owners', 'police_officers', 'departments'], function () {
     seeder.populateModels(data, function () {
       seeder.disconnect()
     })
   })
 })
+
+const officerId = mongoose.Types.ObjectId()
+const departmentId = mongoose.Types.ObjectId()
 
 const data = [
   {
@@ -28,6 +34,32 @@ const data = [
         password: bcrypt.hashSync('123456', 10),
         mobile_number: '34555555555',
         address: 'c/My address here',
+      },
+    ],
+  },
+  {
+    model: 'departments',
+    documents: [
+      {
+        _id: departmentId,
+        name: 'Robos distrito 1',
+        officers: [officerId],
+        assignments: 'robberies',
+      },
+    ],
+  },
+  {
+    model: 'police_officers',
+    documents: [
+      {
+        _id: officerId,
+        name: 'Jose',
+        surname: 'Santana Dominguez',
+        email: 'jose.santana@police.com',
+        password: bcrypt.hashSync('123456', 10),
+        police_license_number: '358467A',
+        role: 'regular',
+        department: departmentId,
       },
     ],
   },
