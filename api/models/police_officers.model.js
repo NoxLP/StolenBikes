5364d115379f5f13aa35705313b27be33b109ea4
Norber <nox_nox_@hotmail.com>
2021-12-22 01:25:40 +0000

    Fix officers model

--- api/models/police_officers.model.js
+++ api/models/police_officers.model.js
@@ -9,6 +9,18 @@ const policeOfficersSchema = new mongoose.Schema({
     type: String,
     required: [true, 'Name is required'],
   },
+  email: {
+    type: String,
+    required: [true, 'Email is required'],
+    validate: {
+      validator(value) {
+        return /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/.test(
+          value
+        )
+      },
+    },
+    unique: [true, 'This is email is registered'],
+  },
   password: {
     type: String,
     required: [true, 'Password is required'],
@@ -24,7 +36,6 @@ const policeOfficersSchema = new mongoose.Schema({
   },
   bike: {
     type: mongoose.Types.ObjectId,
-    required: [true, 'Bike is required'],
     ref: 'bikes',
   },
   department: {
