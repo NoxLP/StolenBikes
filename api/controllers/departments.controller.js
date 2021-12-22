49a671e4371a40a999b849ca7ed991fccdea7add
Norber <nox_nox_@hotmail.com>
2021-12-22 17:56:03 +0000

    Create department endpoint

--- api/controllers/departments.controller.js
+++ api/controllers/departments.controller.js
@@ -1,9 +1,33 @@
+const DepartmentsModel = require('../models/departments.model')
 const { handleError } = require('../utils')
 
-exports.createDepartment = async (req, res) => {}
+exports.createDepartment = async (req, res) => {
+  try {
+    const department = await DepartmentsModel.create(req.body)
 
-exports.getDepartment = async (req, res) => {}
+    return res.status(200).json({ msg: 'department created' })
+  } catch (err) {
+    handleError(err, res)
+  }
+}
 
-exports.updateDepartment = async (req, res) => {}
+exports.getDepartment = async (req, res) => {
+  try {
+  } catch (err) {
+    handleError(err, res)
+  }
+}
 
-exports.removeDepartment = async (req, res) => {}
+exports.updateDepartment = async (req, res) => {
+  try {
+  } catch (err) {
+    handleError(err, res)
+  }
+}
+
+exports.removeDepartment = async (req, res) => {
+  try {
+  } catch (err) {
+    handleError(err, res)
+  }
+}
