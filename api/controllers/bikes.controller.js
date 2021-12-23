b777a275e43e0a18d9a78cde06571f5ed46b2675
Norber <nox_nox_@hotmail.com>
2021-12-23 18:44:26 +0000

    Fix mongoose transactions syntax

--- api/controllers/bikes.controller.js
+++ api/controllers/bikes.controller.js
@@ -7,27 +7,51 @@ const { handleError } = require('../utils')
 
 exports.reportStolenBike = async (req, res) => {
   try {
-    // later we need to add the officer to the bike and other things,
     // use a transaction so it all becomes an atomic operation
     const session = await mongoose.startSession()
     session.startTransaction()
 
     try {
-      const owner = res.locals.owner
       const bikeData = {
         ...req.body,
-        owner: owner._id,
+        owner: res.locals.owner._id,
       }
-      const bike = await BikesModel.create(bikeData)
+      // if it's a new bike, create it (upsert)
+      // if the bike already exists AND is solved, update to unassigned
+      let bike = await BikesModel.findOne({
+        license_number: req.body.license_number,
+      })
+      console.log('> first bike:', bike)
+      if (!bike) {
+        bike = new BikesModel(bikeData)
+      } else if (bike && bike.status == 'solved') {
+        bike.status = 'unassigned'
+      } else {
+        return handleError('already reported bike assigned', res)
+      }
+      console.log('> second bike:', bike)
+      // by not using res.locals.owner but retrieving it from the DB,
+      // mongoose won't save the owner changes until the transaction is commited
+      const owner = await OwnersModel.findById(res.locals.owner._id).session(
+        session
+      )
       owner.bikes.push(bike)
-      await owner.save()
 
       // Assign automatically a free officer to a stolen bike
+
       // get officers of robberies departments
       const robberiesOfficers = await DepartmentsModel.aggregate([
         {
           $match: {
             assignments: 'robberies',
+            $expr: {
+              $lt: [
+                {
+                  $size: '$bike_officers',
+                },
+                '$max_bike_cases',
+              ],
+            },
           },
         },
         {
@@ -43,7 +67,8 @@ exports.reportStolenBike = async (req, res) => {
             },
           },
         },
-      ])
+      ]).session(session)
+      console.log('> robofs: ', robberiesOfficers)
 
       if (robberiesOfficers && robberiesOfficers.length > 0) {
         console.log('> robOfs ok')
@@ -58,30 +83,54 @@ exports.reportStolenBike = async (req, res) => {
             $in: officersIds,
           },
           bike: null,
-        }).limit(1)
+        })
+          .limit(1)
+          .session(session)
+        console.log(freeOfficers)
 
         if (freeOfficers && freeOfficers.length == 1) {
           // free officer found, assign
           const firstFreeOfficer = freeOfficers[0]
+
+          // call DB to get department but don't wait here
+          let department = DepartmentsModel.findById(
+            firstFreeOfficer.department
+          ).session(session)
+
           console.log('> firstFreeOfficer: ', firstFreeOfficer)
           firstFreeOfficer.bike = bike._id
+
+          // set bike officer and status
           bike.officer = firstFreeOfficer._id
           bike.status = 'assigned'
 
-          await Promise.all([firstFreeOfficer.save(), bike.save()])
+          // wait DB and add to department's bike officers
+          // TODO: this should be able to be done onthe below Promise.all in an arrow funtion f.i.
+          //    but it won't work for some reason... study it if I have the time for it
+          department = await department
+          department.bike_officers.push(firstFreeOfficer)
+
+          // save all
+          await Promise.all([
+            firstFreeOfficer.save(),
+            department.save(),
+            bike.save(),
+            owner.save(),
+          ])
 
           await session.commitTransaction()
           return res
             .status(200)
             .json({ msg: 'bike created and assigned', bike })
-        } else {
-          // free officer NOT found
-          await session.commitTransaction()
-          return res
-            .status(200)
-            .json({ msg: 'bike created but NOT assigned', bike })
         }
       }
+
+      // free officer NOT found
+      await Promise.all([bike.save(), owner.save()])
+      await session.commitTransaction()
+      return res
+        .status(200)
+        .json({ msg: 'bike created but NOT assigned', bike })
     } catch (err) {
       handleError(err, res)
     } finally {
