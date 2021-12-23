8da128f8327de11a28510fce085dc56b2269a38f
Norber <nox_nox_@hotmail.com>
2021-12-23 18:45:27 +0000

    Update officer endpoint. Takes care of officers departments change too

--- api/controllers/police_officers.controller.js
+++ api/controllers/police_officers.controller.js
@@ -6,23 +6,23 @@ const { hashSync } = require('bcrypt')
 
 exports.createOfficer = async (req, res) => {
   try {
-    // department is required
-    const department = await DepartmentsModel.findById(
-      res.locals.officer.department
-    )
-    if (!department) return handleError('department not found', res)
-
-    // only admins of same department can create new officers
-    if (department._id != req.body.department)
-      return handleError('incorrect department', res)
-
     // later we need to add the officer to the department,
     // use a transaction so it all becomes an atomic operation
     const session = await mongoose.startSession()
     session.startTransaction()
     try {
-      const encryptedPwd = hashSync(req.body.password, 10)
+      // only admins of same department can create new officers
+      console.log(res.locals.officer.department, req.body.department)
+      if (res.locals.officer.department != req.body.department)
+        return handleError('incorrect department', res)
 
+      // existing department is required
+      const department = await DepartmentsModel.findById(
+        res.locals.officer.department
+      ).session(session)
+      if (!department) return handleError('department not found', res)
+
+      const encryptedPwd = hashSync(req.body.password, 10)
       const officerData = {
         name: req.body.name,
         surname: req.body.surname,
@@ -32,7 +32,8 @@ exports.createOfficer = async (req, res) => {
         role: req.body.role,
         department: req.body.department,
       }
-      const officer = await OfficersModel.create(officerData)
+      const officer = new OfficersModel(officerData)
+      await officer.save()
 
       department.officers.push(officer)
       await department.save()
@@ -46,6 +47,83 @@ exports.createOfficer = async (req, res) => {
     }
   } catch (err) {
     // this should catch the transaction and session creation errors
+    handleError(err, res)
+  }
+}
+
+exports.updateOfficer = async (req, res) => {
+  try {
+    // use a transaction so it all becomes an atomic operation
+    const session = await mongoose.startSession()
+    session.startTransaction()
+
+    try {
+      const promises = [] // will await this at the end with promise.all
+
+      const officer = await OfficersModel.findById(
+        req.params.officerId
+      ).session(session)
+      if (!officer) return handleError('officer not found', res, 404)
+
+      // if department is being changed we must reorganize origin and target departments
+      if (req.body.department) {
+        const targetDepartment = await DepartmentsModel.findById(
+          req.body.department
+        ).session(session)
+        const originDepartment = await DepartmentsModel.findById(
+          officer.department
+        ).session(session)
+
+        if (officer.bike) {
+          if (targetDepartment.assignments !== 'robberies') {
+            return handleError(
+              "can't change officer to a non robberies department while solving a stolen bike case",
+              res
+            )
+          }
+
+          // check if a new officer will break the max bike cases rule
+          if (
+            targetDepartment.bike_officers.length <
+            targetDepartment.max_bike_cases
+          ) {
+            originDepartment.bike_officers.pull(officer)
+            targetDepartment.bike_officers.push(officer)
+          } else {
+            return handleError(
+              'target department have already reached max stolen bike cases',
+              res
+            )
+          }
+        }
+
+        originDepartment.officers.pull(officer)
+        targetDepartment.officers.push(officer)
+        officer.department = targetDepartment._id
+
+        promises.push(originDepartment.save())
+        promises.push(targetDepartment.save())
+      }
+
+      //password, bike and department can't be changed here
+      officer.name = req.body.name ?? officer.name
+      officer.surname = req.body.surname ?? officer.surname
+      officer.email = req.body.email ?? officer.email
+      officer.police_license_number =
+        req.body.police_license_number ?? officer.police_license_number
+      officer.role = req.body.role ?? officer.role
+
+      promises.push(officer.save())
+      await Promise.all(promises)
+
+      await session.commitTransaction()
+      return res.status(200).json({ msg: 'officer updated' })
+    } catch (err) {
+      handleError(err, res)
+    } finally {
+      session.endSession()
+    }
+  } catch (err) {
     handleError(err, res)
   }
 }
--- api/routes/police_officers.router.js
+++ api/routes/police_officers.router.js
@@ -1,7 +1,11 @@
 const router = require('express').Router()
-const { createOfficer } = require('../controllers/police_officers.controller')
-const { officerIsAdmin } = require('../utils/auth') // Authenticated Route
+const {
+  createOfficer,
+  updateOfficer,
+} = require('../controllers/police_officers.controller')
+const { officerIsAdmin } = require('../utils/auth')
 
 router.post('/', officerIsAdmin, createOfficer)
+router.put('/:officerId', officerIsAdmin, updateOfficer)
 
 module.exports = router
